832afa922592a16fb7945d4e5a86a628aba9110d
Audu-Daniels <aududaniels@gmail.com>
2022-06-24 13:12:15 +0100

    complete header and hero section

--- src/component/List.js
+++ src/component/List.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "./Css/ListStyle.css";
 import Button from "./Button";
+import CustomNav from "./CustomNav/CustomNav";
 
 const List = () => {
   return (
@@ -13,39 +14,60 @@ const List = () => {
           </div>
           <nav>
             <div className="list">
-              <NavLink
+              {/* <NavLink
                 to="/"
                 exact
                 className="listItem"
                 activeClassName="activeItem"
               >
                 Home
-              </NavLink>
-              <NavLink
+              </NavLink> */}
+              {/* <NavLink
+                to="/"
+                style={({ isActive }) => (
+                    { color: isActive ? "#4361EE" : 'black',
+                        border: isActive ? 'none' : 'none',
+                        textDecoration: isActive ? "none" : "none",
+                        fontSize: isActive ? "0.5em" : "0.5em",
+                        fontWeight: isActive ? "700" : "700",
+                        fontFamily: isActive ? 'Poppins' : 'Poppins',
+                    }
+                    )
+                }
+                >
+                Home
+                </NavLink> */}
+                <CustomNav path='/'>Home</CustomNav>
+                <CustomNav path='/features'>Features</CustomNav>
+                <CustomNav path='/about'>About</CustomNav>
+                <CustomNav path='/contactUs'>Contact Us</CustomNav>
+                
+              {/* <NavLink
                 to="/features"
                 className="listItem"
                 activeClassName="activeItem"
               >
                 Features
-              </NavLink>
-              <NavLink
+              </NavLink> */}
+              {/* <NavLink
                 to="/about"
                 className="listItem"
                 activeClassName="activeItem"
               >
                 About
-              </NavLink>
-              <NavLink
+              </NavLink> */}
+              {/* <NavLink
                 to="/contactUs"
                 className="listItem"
                 activeClassName="activeItem"
               >
                 ContactUs
-              </NavLink>
+              </NavLink> */}
             </div>
           </nav>
+
           <div className="login-link">
-            <NavLink to="/" className='listItem'>Login</NavLink>
+            <NavLink to="/" className='login'>Login</NavLink>
             <Button title={"Create an account"} color="inherit"></Button>
           </div>
         </div>
