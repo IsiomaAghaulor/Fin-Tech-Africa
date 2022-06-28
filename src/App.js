11cede75db0493a4827ee97cf51967846ccce599
Buezman <chibuezenwajiobi@gmail.com>
2022-06-28 16:43:26 +0100

    first commit for How it works page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import HowItWorks from "./pages/landingpage/HowItWorks";
 import Routes from "./routes/routes"
 
 function App() {
   return (
     <div className="App">
+      <HowItWorks />
       <Routes/>
     </div>
   );
