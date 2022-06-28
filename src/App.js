9fba0e44b26b96d2dd9851cc4e0eb3b587378edf
Buezman <chibuezenwajiobi@gmail.com>
2022-06-28 20:20:57 +0100

    a little adjustment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import HearFromOurCustomers from "./pages/landingpage/HearFromOurCustomers";
-import HowItWorks from "./pages/landingpage/HowItWorks";
 import Routes from "./routes/routes"
 
 function App() {
   return (
     <div className="App">
-      <HowItWorks />
-      <HearFromOurCustomers />
       <Routes/>
     </div>
   );
