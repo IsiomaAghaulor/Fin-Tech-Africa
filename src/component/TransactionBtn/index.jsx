4d2591cb5a3fc152073634c163a7c92da9c57a37
teddyrugged <jakpaetuk@gmail.com>
2022-06-30 15:13:04 +0100

    updated hover

--- src/component/TransactionBtn/index.jsx
+++ src/component/TransactionBtn/index.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import './index.css';
+import React from "react";
+import "./index.css";
 
-export const TransactionBtn = () => (
-    <div className='TransactionBtn'>
-        <button className='transfer'>Transfer</button>
-        <button className='profile'> Profile</button>
+export const TransactionBtn = () => {
+  return (
+    <div className="TransactionBtn">
+      <button className="transfer">Transfer</button>
+      <button className="profile"> Profile</button>
     </div>
-);
+  );
+};
