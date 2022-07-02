9925f0af221e47f2e23be058d323854c8026c588
teddyrugged <jakpaetuk@gmail.com>
2022-07-02 01:26:48 +0100

    update

--- src/component/TransactionBtn/index.jsx
+++ src/component/TransactionBtn/index.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./index.css";
 
 export const TransactionBtn = () => {
+
+  const navigate = useNavigate()
+
+ 
   return (
     <div className="TransactionBtn">
-      <button className="transfer">Transfer</button>
+      
+        <button onClick={() => navigate("/")} className="transfer">Transfer</button>
+  
       <button className="profile"> Profile</button>
     </div>
   );
