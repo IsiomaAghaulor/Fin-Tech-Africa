5a6b0ce0131d4db316e4fb60a601a1409f9980d6
teddyrugged <jakpaetuk@gmail.com>
2022-07-14 18:23:39 +0100

    refactor

--- src/pages/home/landingPage.js
+++ src/pages/home/landingPage.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Footer from "./Footer";
 import "./landingPage.css";
 import NavBar from "./navBar/navBar"
 import Section1 from "./section1/section1";
 import Section2 from "./section2/section2";
 import Section3 from "./section3/section3";
 import Section4 from "./section4/section4";
+import Section5 from "./section5/index";
+import Section6 from "./section6/index";
 
 const LandingPage = () => {
   return (
@@ -24,6 +27,15 @@ const LandingPage = () => {
         </div>
         <div>
           <Section4 />
+        </div>
+        <div>
+          <Section5 />
+        </div>
+        <div>
+          <Section6 />
+        </div>
+        <div>
+          < Footer />
         </div>
       </div>
     </div>
