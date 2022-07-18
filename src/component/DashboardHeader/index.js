15b466b3a3c4da442e7b448bf5404f891f96c771
teddyrugged <jakpaetuk@gmail.com>
2022-07-18 21:53:37 +0100

    responsiveness

--- src/component/DashboardHeader/index.js
+++ src/component/DashboardHeader/index.js
@@ -1,61 +1,62 @@
-import React, { useState } from 'react';
-import './index.css';
-import { ReactComponent as Pic } from './images/pic.svg';
-import { ReactComponent as Bell } from './images/bell.svg';
-import { ReactComponent as Red  } from './images/red.svg';
-import { FiSettings } from "react-icons/fi"
+import React, { useState } from "react";
+import "./index.css";
+import { ReactComponent as Pic } from "./images/pic.svg";
+import { ReactComponent as Bell } from "./images/bell.svg";
+import { ReactComponent as Red } from "./images/red.svg";
+import { FiSettings } from "react-icons/fi";
 import { FaBars, FaTimes } from "react-icons/fa";
-import {  NavLink } from 'react-router-dom';
-
+import { NavLink } from "react-router-dom";
 
 const DashboardHeader = () => {
-    const [click, setClick] = useState(false);
+  const [click, setClick] = useState(false);
 
-    const handleClick = () => {
-        setClick(!click);
-      };
+  const handleClick = () => {
+    setClick(!click);
+  };
 
-   return (
-<nav className="navbar_b ">
+  return (
+    <nav className="navbar_b ">
       <div className="container navbar-wrapper">
         <div className="navbar__logo">
-          <span >
-           <h1>Fintech.africa</h1>
+          <span>
+            <h1>Fintech.africa</h1>
           </span>
         </div>
         <div className="menu-icon" onClick={handleClick}>
           {click ? <FaTimes className="icons" /> : <FaBars className="icons" />}
         </div>
-          <div className={click ? "navbar__right active" : "navbar__right"}>
-            <ul className="nav-links">
-              {/* {menuItems.map((item) => ( */}
-                <li>
-                  <NavLink exact={true} className="li sett" to="/">
-                  <FiSettings />
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink exact={true} className="li atBell" to="#">
-                  <Bell />
-                  <span className='sideBadge'><Red /></span>
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink exact={true} className="li" to="#">
-                  <Pic />
-                  </NavLink>
-                </li>
-                <li>
-                  <NavLink exact={true} className="li" to="/">
-                  <span>Adetula</span>
-                  </NavLink>
-                </li>
-              {/* ))} */}
-            </ul>
-          </div>
+        <div className={click ? "navbar__right active" : "navbar__right"}>
+          <ul className="nav-links">
+            {/* {menuItems.map((item) => ( */}
+            <li>
+              <NavLink exact={true} className="li sett" to="/">
+                <FiSettings />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink exact={true} className="li atBell" to="#">
+                <Bell />
+                <span className="sideBadge">
+                  <Red />
+                </span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink exact={true} className="li" to="#">
+                <Pic />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink exact={true} className="li" to="/">
+                <span>Adetula</span>
+              </NavLink>
+            </li>
+            {/* ))} */}
+          </ul>
+        </div>
       </div>
     </nav>
-   )
-   };
+  );
+};
 
 export default DashboardHeader;
